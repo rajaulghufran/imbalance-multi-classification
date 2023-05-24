5746dab7bf6bc8371c3e0cba07e30523850208ae
Muhammad Rajaul Ghufran <40607028+rajaulghufran@users.noreply.github.com>
2023-05-25 00:04:02 +0700

    Update util.py
    - move POS to util

--- util.py
+++ util.py
@@ -12,6 +12,12 @@ from pandas.api.types import (
 
 from pipeline.classification import Classification
 
+POS = {
+    "tags": ["ADJ","ADP","ADV","AUX","CONJ","DET","INTJ","NOUN","NUM","PART","PRON","PROPN","PUNCT","SCONJ","SYM","VERB","X"],
+    "descriptions": ["adjective","adposition","adverb","auxiliary","conjunction","determiner","interjection","noun","numeral","particle","pronoun","proper noun","punctuation","subordinating conjunction","symbol","verb","other"],
+    "examples": ["salah, pertama, besar","di, pada, dari","juga, lebih, kemudian","adalah, akan, dapat","dan, atau, tetapi","ini, itu, buah","Hai, Oh, Sayang","tahun, orang, desa","satu, dua, 1","tidak, kah, lah","yang, dia, mereka","Indonesia, kabupaten, kecamatan",", ? ()","untuk, bahwa, dengan","%, =, °","menjadi, merupakan, memiliki", "and, image, in"]
+}
+
 def init_state(name: str, val: any) -> None:
     if name not in st.session_state:
         st.session_state[name] = val
