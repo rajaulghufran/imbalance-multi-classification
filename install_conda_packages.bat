ed51f2f82dee1bd86498da258f77c4a50de9010c
Muhammad Rajaul Ghufran <40607028+rajaulghufran@users.noreply.github.com>
2023-06-09 04:29:56 +0700

    Update install_conda_packages.bat
    - remove unused library

--- install_conda_packages.bat
+++ install_conda_packages.bat
@@ -3,9 +3,6 @@ CALL conda update -n base -c defaults conda -y
 CALL conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y
 CALL conda install -c stanfordnlp -c conda-forge stanza -y
 CALL conda install -c conda-forge scikit-learn -y
-CALL conda install -c conda-forge scikit-learn-intelex -y
-CALL conda install dpctl -c intel -y
-CALL conda install dpcpp_cpp_rt -c intel -y
 CALL conda install -c conda-forge matplotlib -y
 CALL conda install -c conda-forge ipywidgets -y
 CALL conda install -c conda-forge streamlit -y
